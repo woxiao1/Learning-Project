ba2ba7699fb8b0b0614c658e962af7718dd2b9d0
Shiao <baohcm36@gmail.com>
2024-07-09 11:16:46 +0700

    add update project

---
+++ Assets/Scripts/So1Script.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts
+{
+    public class So1Script : MonoBehaviour
+    {
+
+        public float a, b, c;
+
+        void Start()
+        {
+            float delta, x1, x2;
+
+            delta = b * b - 4 * a * c;
+
+            if (delta > 0)
+            {
+
+                x1 = ((float)(-b + Math.Sqrt(delta))) / (2 * a);
+                x2 = ((float)(-b - Math.Sqrt(delta))) / (2 * a);
+
+                Debug.Log($"Phương trình có 2 nghiệm : {x1}, {x2}");
+            }
+
+            else if (delta == 0)
+            {
+
+                x1 = x2 = -b / (2 * a);
+
+                Debug.Log($"Phương trình có nghiệm kép : {x1}");
+
+            }
+
+            else
+            {
+                Debug.Log("Phương trình vô nghiệm");
+            }
+
+        }
+    }
+}
