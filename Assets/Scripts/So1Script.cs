b2745b78028c5f40bc481ad285b6db929de5c158
Dan Pham <phamphuc0603@gmail.com>
2024-07-09 12:10:43 +0700

    Fix So1Script and delete Cailz.txt

--- Assets/Scripts/So1Script.cs
+++ Assets/Scripts/So1Script.cs
@@ -7,38 +7,29 @@ namespace Assets.Scripts
 {
     public class So1Script : MonoBehaviour
     {
-
-        public float a, b, c;
+        public float a;
+        public float b;
+        public float c;
 
         void Start()
-        {
-            float delta, x1, x2;
-
-            delta = b * b - 4 * a * c;
-
+        { 
+            float delta = b * b - 4 * a * c;
             if (delta > 0)
             {
-
-                x1 = ((float)(-b + Math.Sqrt(delta))) / (2 * a);
-                x2 = ((float)(-b - Math.Sqrt(delta))) / (2 * a);
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
 
                 Debug.Log($"Phương trình có 2 nghiệm : {x1}, {x2}");
             }
-
             else if (delta == 0)
             {
-
-                x1 = x2 = -b / (2 * a);
-
-                Debug.Log($"Phương trình có nghiệm kép : {x1}");
-
+                double x = -b / (2 * a);
+                Debug.Log($"Phương trình có nghiệm kép : {x}");
             }
-
             else
             {
                 Debug.Log("Phương trình vô nghiệm");
             }
-
         }
     }
 }
