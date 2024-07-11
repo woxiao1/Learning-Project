51f9125385cee853321ceddbfe186ae403b7c039
Shiao <baohcm36@gmail.com>
2024-07-11 10:34:00 +0700

    add coin and scoretext

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class PlayerScore : MonoBehaviour
+{
+    public TMP_Text Scoretext;
+    public int score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        score += 1;
+        Scoretext.text = $"Score : {score}";
+
+        
+    }
+
+
+
+
+}
